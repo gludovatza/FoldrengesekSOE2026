2d35b8737d4b651c20bc3d488b2ed58d2921a2f6
gludovatza <attila@gludovatz.hu>
2026-04-01 19:23:18 +0200

    FeladatokController, indexe és a Feladat2 nézet

---
+++ FoldrengesekSOE2026/Controllers/FeladatokController.cs
@@ -0,0 +1,31 @@
+﻿using FoldrengesekSOE2026.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoldrengesekSOE2026.Controllers
+{
+    public class FeladatokController : Controller
+    {
+        private readonly FoldrengesContext _context;
+
+        public FeladatokController(FoldrengesContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Feladat2()
+        {
+            var results = _context.Telepulesek
+                .Where(t => t.Varmegye == "Somogy")
+                .OrderBy(t => t.Nev)
+                .Select(t => t.Nev);
+
+            return View(results);
+
+        }
+    }
+}
